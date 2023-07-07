64a762c2e446c8e2db8be75cb7586be1b82cc507
Agnes Kaswii <agneskaswii700@gmail.com>
2023-07-07 12:04:10 +0300

    deploy

--- solarstique/urls.py
+++ solarstique/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authapp.urls')),
     path('', include('customers.urls')),
-    # path('', include('carbon.urls')),
+    path('', include('carbon.urls')),
     path('', include('products.urls')),
 ]
